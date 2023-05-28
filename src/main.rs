21a44f242df0af46ed3d509f6299ee3959878bc9
Jack <jack.morgan.edwards@gmail.com>
2023-05-28 18:51:34 -0400

    mutable chord methods

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ fn main() {
     let d = PitchClass::new(NoteName::D);
     let gb = PitchClass::new(NoteName::Gb);
     let a = PitchClass::new(NoteName::A);
-
-    let _bosh = NoteSet {
-        set: vec![d, gb, a],
+    let d2 = PitchClass::new(NoteName::D);
+    let mut bosh = NoteSet {
+        set: vec![d, gb, a, d2],
     };
     // let d_new = PitchClass::new(NoteName::D);
     // let size_vec = vec![2, 2, 2];
-    let chord = Chord { pitch_list: _bosh };
-    chord.print_chord();
-    let new = chord.invert();
-    new.print_chord();
+    // let chord = Chord { pitch_list: _bosh };
+    // chord.print_chord();
+    // let new = chord.invert();
+    // new.print_chord();
+    bosh.print_set();
     // let new_set = bosh.get_transposed(0);
 
     // new_set.print_set();
--- src/note_set.rs
+++ src/note_set.rs
@@ -6,16 +6,29 @@ pub struct NoteSet {
 }
 
 impl NoteSet {
-    pub fn get_transposed(&self, num: i32) -> NoteSet {
+    pub fn tranpose(&mut self, amt: i32) {
+        for i in &mut self.set {
+            i.transpose(amt);
+        }
+    }
+
+    pub fn get_transposed(&self, amt: i32) -> NoteSet {
         let mut new_set: Vec<PitchClass> = vec![];
 
         for i in self.set.iter() {
             let mut note = PitchClass { value: i.value };
-            note.transpose(num);
+            note.transpose(amt);
             new_set.push(note);
         }
 
         return NoteSet { set: new_set };
+    }
+
+    pub fn transpose_to_0(&mut self) {
+        let amt = self.set[0].value;
+        for i in &mut self.set {
+            i.transpose(-amt);
+        }
     }
 
     pub fn get_transposed_to_0(&self) -> NoteSet {
@@ -31,7 +44,18 @@ impl NoteSet {
         return NoteSet { set: new_set };
     }
 
-    pub fn remove_duplicates(&self) -> NoteSet {
+    pub fn remove_duplicates(&mut self) {
+        let mut new_set: Vec<PitchClass> = vec![];
+        for i in self.set.iter() {
+            if !new_set.contains(i) {
+                new_set.push((*i).clone());
+            }
+        }
+
+        self.set = new_set;
+    }
+
+    pub fn get_duplicates_removed(&self) -> NoteSet {
         let mut new_set: Vec<PitchClass> = vec![];
         for i in self.set.iter() {
             if !new_set.contains(i) {
